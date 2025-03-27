c195371dbb52ea039d09d68fea9ace8e8ec988eb
Reed <reedwrogers@gmail.com>
2025-03-27 17:50:22 -0500

    updates

---
+++ Project_4/utils/.ipynb_checkpoints/io-checkpoint.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+""" Contains data reading / writing for imageries """
+
+import cv2
+import numpy as np
+import imageio
+
+
+def write_image(image:np.ndarray, image_path: str):
+    '''
+    Writes image from image path
+    Args:
+        image: RGB image of shape H x W x C, with float32 data
+        image_path: path to image
+
+    Returns:
+        RGB image of shape H x W x 3 in floating point format
+    '''
+    # read image and convert to RGB
+    bgr_image = (image[:, :, [2, 1, 0]] * 255).astype(np.uint8)
+    cv2.imwrite(image_path, bgr_image)
+
+
+def read_image(image_path: str) -> np.ndarray:
+    '''
+    Reads image from image path, and 
+    return floating point RGB image
+    
+    Args:
+        image_path: path to image
+
+    Returns:
+        RGB image of shape H x W x 3 in floating point format
+    '''
+    # read image and convert to RGB
+    bgr_image = cv2.imread(image_path)
+    rgb_image = bgr_image[:, :, [2, 1, 0]]
+    return rgb_image.astype(np.float32) / 255
+
+
+def read_hdr_image(image_path: str) -> np.ndarray:
+    '''
+    Reads image from image path, and 
+    return HDR floating point RGB image
+    
+    Args:
+        image_path: path to hdr image
+
+    Returns:
+        RGB image of shape H x W x 3 in floating point format
+    '''
+    
+    # Read HDR image using imageio
+    hdr_image = imageio.imread(image_path)
+    
+    # Ensure the image is in the expected format
+    if len(hdr_image.shape) == 2:
+        # If the image is grayscale (2D), stack to convert to RGB by duplicating the channel
+        rgb_hdr_image = np.stack((hdr_image,) * 3, axis=-1)
+    elif hdr_image.shape[2] == 3:
+        # If the image has 3 channels, assume it is already in RGB format
+        rgb_hdr_image = hdr_image  # No need to convert
+    elif hdr_image.shape[2] == 4:
+        # If there is an alpha channel, discard it by taking only the first three channels (RGB)
+        rgb_hdr_image = hdr_image[:, :, :3]
+
+    # Convert to floating point format
+    return rgb_hdr_image.astype(np.float32)
+
+def write_hdr_image(hdr_image: np.ndarray, image_path: str):
+    '''
+    Write HDR image to given path.
+    The path must end with '.hdr' extension
+    Args:
+        hdr_image: H x W x C float32 HDR image in BGR format.
+        image_path: path to hdr image to save
+
+    Returns:
+        RGB image of shape H x W x 3 in floating point format
+    '''
+    assert(image_path.endswith('.hdr'))
+    rgb_hdr_image = hdr_image[:, :, [2, 1, 0]]
+    cv2.imwrite(image_path, rgb_hdr_image)
